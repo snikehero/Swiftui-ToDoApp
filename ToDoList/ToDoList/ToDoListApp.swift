96cfe65ff07b7b451986e10bf8e5ffbfc16acd03
snikehero <snikehero234@gmail.com>
2024-02-02 16:16:56 -0600

    Added FireBase To project
    Added FireBase to the TodoApp

--- ToDoList/ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -4,11 +4,14 @@
 //
 //  Created by Moises Sanchez on 02/02/24.
 //
-
+import FirebaseCore
 import SwiftUI
 
 @main
-struct ToDoListApp: App {
+struct ToDoListApp: App{
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
