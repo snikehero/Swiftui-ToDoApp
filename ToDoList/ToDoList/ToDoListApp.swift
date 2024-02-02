4b4b1a58ca804d4f61c57886538734de48122268
snikehero <snikehero234@gmail.com>
2024-02-02 15:49:07 -0600

    FirstCommit

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Moises Sanchez on 02/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
