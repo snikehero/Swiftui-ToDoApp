a42e29203f33d2ec5ffa7ff4c395f85af75929dc
snikehero <snikehero234@gmail.com>
2024-02-03 14:27:41 -0600

    Applied ViewBuilder to ToDoListView

--- ToDoList/ToDoList/Views/ContentView.swift
+++ ToDoList/ToDoList/Views/ContentView.swift
@@ -12,18 +12,23 @@ struct ContentView: View {
     var body: some View {
         if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty {
             //Signed in
-            TabView {
-                ToDoListView()
-                    .tabItem {
-                    Label("Home", systemImage: "house")
-                    }
-                ProfileView()
-                    .tabItem {
-                        Label("Profile", systemImage: "person.circle")
-                    }
-            }
+            accountView
         } else {
             LoginView()
+        }
+    }
+    
+    @ViewBuilder
+    var accountView: some View {
+        TabView {
+            ToDoListView(userId: viewModel.currentUserId)
+                .tabItem {
+                Label("Home", systemImage: "house")
+                }
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person.circle")
+                }
         }
     }
 }
--- ToDoList/ToDoList/Views/ToDoListView.swift
+++ ToDoList/ToDoList/Views/ToDoListView.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 struct ToDoListView: View {
     @StateObject var viewModel = TodoListViewViewModel()
-    
-    init() {
-        
+    private let userId: String
+    init(userId: String) {
+        self.userId = userId
     }
     var body: some View {
         NavigationView {
@@ -33,6 +33,6 @@ struct ToDoListView: View {
 
 struct ToDoListView_Previews: PreviewProvider {
     static var previews: some View {
-        ToDoListView()
+        ToDoListView(userId: "")
     }
 }
