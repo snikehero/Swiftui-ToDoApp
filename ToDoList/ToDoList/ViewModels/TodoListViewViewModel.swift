1adc9dd57b4dcb1258816ae0e0cd7e30302b6c8b
snikehero <snikehero234@gmail.com>
2024-02-03 14:46:08 -0600

    Created NewItemView Calendar

--- ToDoList/ToDoList/ViewModels/TodoListViewViewModel.swift
+++ ToDoList/ToDoList/ViewModels/TodoListViewViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
     // ViewModel for list of items view
     // Primary Tab
 class TodoListViewViewModel: ObservableObject {
+    @Published var showingNewItemView = false 
     init() {
         
     }
