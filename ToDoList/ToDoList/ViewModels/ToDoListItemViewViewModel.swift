c5acad420ef7220d1a7dd54771ac23782e456f3c
snikehero <snikehero234@gmail.com>
2024-02-05 17:38:05 -0600

    Implementation of LogOut

--- ToDoList/ToDoList/ViewModels/ToDoListItemViewViewModel.swift
+++ ToDoList/ToDoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -4,12 +4,27 @@
 //
 //  Created by Moises Sanchez on 02/02/24.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 //ViewModel for single todo list item view (each row in items list)
 class ToDoListItemViewViewModel: ObservableObject {
     init() {
         
+    }
+    func toogleIsDone(item: ToDoListItem) { //Updating the isDone property of the model both in Swift and FireStore DB.
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
     }
 }
